1681db2ce267167e6bc5eb8de48120467c34dfee
younghyunlee22 <younghyunlee22@gmail.com>
2023-06-03 16:09:02 -0500

    Brute Force

---
+++ 1.two-sum.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        result = []
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    result.append(i)
+                    result.append(j)
+        return result
+        
+        
+# @lc code=end
+
